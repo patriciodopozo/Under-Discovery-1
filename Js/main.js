d3578971f9d4ca2488d4d8f9b40d4ae03549243a
Jhon Erick <erickbienn@gmail.com>
2021-08-22 18:59:16 -0300

    Agregando slider y correción en el scrool

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.galeria').bxSlider({
+        mode: 'fade',
+        captions: false,
+        slideWidth: 1000,
+        responsive: true,
+        pager: true
+      });
+});
